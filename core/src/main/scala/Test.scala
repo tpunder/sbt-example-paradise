ea499cdf32cec55a5c53c23ba22f17a01040af4f
Tim Underwood <timunderwood@gmail.com>
2014-03-27 07:50:20 -0700

    Shorten the output

--- core/src/main/scala/Test.scala
+++ core/src/main/scala/Test.scala
@@ -1,7 +1,7 @@
 object Test extends App {
-  println("Compile Time Annotations for ignoredField3: "+helloMacro.compileTimeAnnotations[JavaBean])
+  println("Compile: "+helloMacro.compileTimeAnnotations[JavaBean])
   
   import reflect.runtime.universe._
-  
-  println("Runtime Annotations for ignoredField3: "+typeOf[JavaBean].declaration(newTermName("ignoredField3")).annotations)
+
+  println("Runtime: "+typeOf[JavaBean].declaration(newTermName("ignoredField3")).annotations)
 }
